292104945d6d891cacd68ba127e0f7440e50d5da
Tommyyoliver <tommyolivr@gmail.com>
2026-02-21 19:01:36 -0300

    add angular material && fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
+    { 
+        path: 'home', 
+        loadComponent: () => import('./home/home').then(m => m.Home),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./home/hero-list/hero-list').then(m => m.HeroList),
+            },
+            {
+                path: 'hero/:id',
+                loadComponent: () => import('./home/hero-list/hero-item/hero-item').then(m => m.HeroItem),
+            },
+        ],
+     },
+
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-
-    { path: 'home', loadComponent: () => import('./home/home').then(m => m.Home) },
-
-    { path: 'home/hero/:id', loadComponent: () => import('./home/hero-list/hero-item/hero-item').then(m => m.HeroItem) },
-
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
 
 ];
