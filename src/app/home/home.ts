333b85b3ba2c3b75eef96c7eb012b011e46bef0c
Tommyyoliver <tommyolivr@gmail.com>
2026-02-22 17:11:18 -0300

    implemented home styles

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
